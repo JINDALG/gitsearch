8ec5f1a411f01c69815c36147f3ad17831c7c4b1
shivji <jindalshivam65@gmail.com>
2017-04-28 18:43:42 +0530

    Added filter on admin panel

--- git/admin.py
+++ git/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
+from daterange_filter.filter import DateRangeFilter
+
 from git.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['get_image'] + [f.name for f in User._meta.fields]
+    search_fields = ['login']
+    list_filter = [('created', DateRangeFilter)]
