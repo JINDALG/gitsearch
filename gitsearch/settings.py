68f241661ef2f3e15ed49d5e7649f05e48b068b7
shivji <jindalshivam65@gmail.com>
2017-04-29 19:10:58 +0530

    Added STATICFILES_DIRS in Settings

--- gitsearch/settings.py
+++ gitsearch/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
@@ -135,3 +136,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 REQUEST_IGNORE_PATHS = (
     r'^admin/',
 )
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
