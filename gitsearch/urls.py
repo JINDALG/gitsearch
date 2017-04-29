dae4432aa50a2ccea098c51de9210abff650115c
shivji <jindalshivam65@gmail.com>
2017-04-29 20:12:14 +0530

    added static_url

--- gitsearch/urls.py
+++ gitsearch/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include, patterns
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('git.urls'))
 ]
+
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
